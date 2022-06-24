5199f19e202d4362919b34f2de65b19d470323e7
charleyWarley <ch.jacobs95@gmail.com>
2022-06-24 12:45:58 -0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function howMany() {
+    prompt("How many?");
+}
+
+function submit(){
+    if (confirm("Are you sure you want to submit? Click OK to confirm.") == true) {
+        alert("You've submitted your service request form.")
+    }
+}
+
+function exclude(item) {
+    if (confirm("You've opted to exclude this item. Click OK to confirm.") == true) {
+        document.getElementById(item).innerHTML = "";
+    }
+}
